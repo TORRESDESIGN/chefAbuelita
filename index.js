c9363e532f01f981b6e2b86e030f98dc67ee9a3c
TORRESDESIGN <Daniel.Torres.84@gmail.com>
2025-04-17 17:21:33 -0700

    removed axios from npm package.json, and got the ai request workinig, just need clean up code and have it rendered to front end.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
+//backend server
 const PORT = 8000;
 import express from 'express';
 import cors from 'cors';
 import bodyParser  from 'body-parser';
-//import axios from 'axios';
 import Anthropic from "@anthropic-ai/sdk";
 import 'dotenv/config';
 
@@ -12,31 +12,57 @@ const corsOptions = {
 };
 
 // Add middleware
-//app.use(express.json());
-//app.use(cors());
 app.use(cors(corsOptions));
-app.use(bodyParser.json());
+app.use(bodyParser.json());// newest way to use middleware instead of express.json()
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//The A.I. sauce
 const SYSTEM_PROMPT = process.env.PROMPT;
+const anthropic = new Anthropic({
+    // for ANTHROPIC_API_KEY
+    apiKey: process.env.ANTHROPIC_API_KEY,
+})
 
-const ingredientsArr = ["tortillas", "cheese", "onions"];
+//const ingredientsArr = ["tortillas", "cheese", "onions"]; static, can delete
 
 app.get('/', (req, res) => {
     res.json({
-        Ingredients: ingredientsArr
+        Ingredients: "none"
     })
 })
 
-app.post('/recipe', (req, res) => {
+app.post('/recipe', async (req, res) => {
     const ingredients = req.body;
-    console.log(`I got your ingredients of ${ingredients}, let me think of a recipe hijo mio...`)
+    console.log(`I got your ingredients of ${ingredients}, let me think of a recipe hijo mio...`);
 
-    res.json({
+    try {
+        const results = await getRecipeFromChefClaude(ingredients);
+
+        res.json({
         success: true,
-        message: "completed",
+        message: results,
         data: ingredients
-    })
+        })
+    } catch(error) {
+        console.log("Error: ", error)
+    }
+
+    //*
+    async function getRecipeFromChefClaude(ingredients) {
+
+    const ingredientsString = ingredients.join(", ")
+
+    const msg = await anthropic.messages.create({
+        model: "claude-3-haiku-20240307",
+        max_tokens: 1024,
+        system: SYSTEM_PROMPT,
+        messages: [
+            { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
+        ],
+    });
+    return msg.content[0].text
+    }
+    //*
 })
 /*
 app.get('/', async (req, res) => {
